1b78f5492abd90b2852cc1c155da88ac7eac5ea2
Macarena Acosta <macarenaa988@gmail.com>
2022-07-11 17:22:55 -0300

    adding profesor entity

---
+++ src/main/java/entities/Profesor.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.util.Objects;
+
+public class Profesor {
+    private Long id = null;
+    private String nombre;
+    private String apellido;
+
+    public Profesor(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Profesor profesor = (Profesor) o;
+        return Objects.equals(id, profesor.id) && Objects.equals(nombre, profesor.nombre) && Objects.equals(apellido, profesor.apellido);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre, apellido);
+    }
+}
